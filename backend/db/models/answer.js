7abbad5ebf08e189fb941ce6ebe3de1febd25314
Arjun <arjun.narain700@gmail.com>
2020-12-30 15:05:40 -0600

    question

--- backend/db/models/answer.js
+++ backend/db/models/answer.js
@@ -4,12 +4,10 @@ module.exports = (sequelize, DataTypes) => {
     body: DataTypes.TEXT,
     userId: DataTypes.INTEGER,
     questionId: DataTypes.INTEGER,
-    upVote: DataTypes.INTEGER,
-    downVote: DataTypes.INTEGER
   }, {});
   Answer.associate = function(models) {
     Answer.belongsTo(models.User, { foreignKey: 'userId' });
-  
+
 
     // associations can be defined here
   };
