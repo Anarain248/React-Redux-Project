e74bd2671efa7b99b588eeb8721722ab7482ecf4
Arjun <arjun.narain700@gmail.com>
2021-01-03 14:33:21 -0600

    projectt

--- frontend/src/components/Question/question.js
+++ frontend/src/components/Question/question.js
@@ -5,20 +5,19 @@ import {useSelector} from 'react-redux'
 
 
 const Question = ({question}) => {
-    const answers = useSelector(state => state.answers);
-    // const filteredAnswer = answers.filter((answer) => {
-    //     return answer.questionId === question.id
-    // })
+    // const answers = useSelector(state => state.answers);
+    // // const filteredAnswer = answers.filter((answer) => {
+    // //     return answer.questionId === question.id
+    // // })
     return (
         <>
         <Link to={`/question/${question.id}`}>
         <h2 className='question'>{question.body}</h2>
         </Link>
        { console.log(question)}
-        {/* {question.Answers && filteredAnswer =>
-
-            return <Answer answer={answer}/>
-        } */}
+        {question.Answers && question.Answers.map((answer) => {
+            return <p>{answer.body}</p>
+        })}
         </>
     )
 }
